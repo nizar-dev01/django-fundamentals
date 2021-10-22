77c175934ed542df94e8bce1860ee4570456bddc
nizar <nizar.dev01@gmail.com>
2021-10-22 18:12:27 +0530

    added setup script for later reference

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get update && \
+sudo apt-get install \
+    build-essential \
+    git \
+    python3 \
+    python3-pip \
+    python3-venv \
+    nginx -y \
+&& pip3 install uwsgi
+python3 -m venv ~/app;
+source ~/app/bin/activate
+pip3 install django
+git clone https://github.com/nizar-dev01/baby-django.git
+sudo cp ~/baby-django/server.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/server.conf /etc/nginx/sites-enabled
+sudo service nginx restart
+# Test server
+# uwsgi --socket ~/baby-django/bdj.sock --module app.wsgi --chmod-socket=666
+# Init the actual server
+uwsgi --ini ~/baby-django/app_uwsgi.ini
+
+# Complete the configuration
+mkdir ~/app/vassals
+uwsgi --emperor ~/app/vassals --uid www-data --gid www-data
+

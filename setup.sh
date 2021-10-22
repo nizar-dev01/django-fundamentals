ff2dc70b2af72a3c9b95ab94c8c746feb22bd63d
nizar <nizar.dev01@gmail.com>
2021-10-22 18:53:25 +0530

    Corrected some mistakes

--- setup.sh
+++ setup.sh
@@ -15,9 +15,11 @@ pip3 install django
 git clone https://github.com/nizar-dev01/baby-django.git
 sudo cp ~/baby-django/server.conf /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/server.conf /etc/nginx/sites-enabled
+
+sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
 sudo service nginx restart
 # Test server
-# uwsgi --socket ~/baby-django/bdj.sock --module app.wsgi --chmod-socket=666
+# uwsgi --socket ~/baby-django/app.sock --module app.wsgi --chmod-socket=666
 # Init the actual server
 uwsgi --ini ~/baby-django/app_uwsgi.ini
 

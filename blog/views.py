8e706baf7f236fac16a0251de39edd7202b32976
nizar <nizar.dev01@gmail.com>
2022-12-19 15:29:17 +0530

    Added templates and static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def starting_page(request):
+    return render(request, "blog/index.html", {
+        "title": "First Blog",
+        "content": "Blog content of the first blog post."
+    })
+
+def posts(request):
+    pass
+
+def single_post(request):
+    pass

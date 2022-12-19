004d4caefb41680f80f29230a8a5a588e519bb74
nizar <nizar.dev01@gmail.com>
2022-12-19 14:08:20 +0530

    Deleted boilerplate code

--- scripts/install_system__deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install build-essential git python3 python3-pip python3-venv nginx -y
-pip3 install uwsgi
-systemctl start nginx.service
--- scripts/setup_uwgi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-uwsgi --ini /home/ubuntu/baby-django/app_uwsgi.ini
-mkdir /home/ubuntu/app/vassals
-cp /home/ubuntu/baby-django/emperor.uwsgi.service /etc/systemd/system
-systemctl enable emperor.uwsgi.service
-systemctl start emperor.uwsgi.service
-service nginx restart
